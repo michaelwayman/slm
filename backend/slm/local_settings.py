85765ae71d9723de98fef6656df603d001163656
swiss <nickarnold23@gmail.com>
2017-04-02 23:53:55 -0400

    oopsie

--- backend/slm/local_settings.py
+++ backend/slm/local_settings.py
@@ -1,11 +1,2 @@
 from .settings import *
 import better_exceptions
-
-#
-# INSTALLED_APPS += [
-#     'corsheaders'
-# ]
-#
-# MIDDLEWARE = ['corsheaders.middleware.CorsMiddleware'] + MIDDLEWARE
-#
-# CORS_ORIGIN_ALLOW_ALL = True
